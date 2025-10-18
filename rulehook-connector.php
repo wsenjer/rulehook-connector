003b64bef973a6407f474f8405990708fe486b62
Waseem Senjer <waseem.senjer@gmail.com>
2025-10-18 18:49:21 +0200

    Update .gitignore to exclude rulehook-connector.zip and change text domain to rulehook-connector in rulehook-connector.php

--- rulehook-connector.php
+++ rulehook-connector.php
@@ -15,7 +15,7 @@
  * Author URI:        https://wpruby.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       rulehook
+ * Text Domain:       rulehook-connector
  * Domain Path:       /languages
  */
 
